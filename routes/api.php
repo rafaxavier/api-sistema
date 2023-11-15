5f4d84585fd6d1325451b33f17d802841030bb83
rafaxavier <rxn.90s@gmail.com>
2023-11-15 19:15:32 -0300

    feat: criando rota para pagina de verificacao de email e passando rotas auth para o diretorio das rotas api

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/verified{verified}', function ($parametro) {
+    return view('verified', ['parametro' => $parametro]);
+})->name('verified');
+
+Route::get('/', function () {
+    return ['Laravel' => app()->version()];
+});
+
+require __DIR__ . '/auth.php';
