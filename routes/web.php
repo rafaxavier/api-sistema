26d539816088c7c75428dafebea6ce3999c27404
rafaxavier <rxn.90s@gmail.com>
2023-11-15 19:11:44 -0300

    chore: mudando para contexto de api

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return ['Laravel' => app()->version()];
+// });
+
+// require __DIR__.'/auth.php';
