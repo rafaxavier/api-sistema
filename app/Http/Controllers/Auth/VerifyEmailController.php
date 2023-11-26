44f5a0e0312667fa994455eb4f7592225b03db43
rafaxavier <rxn.90s@gmail.com>
2023-11-26 01:04:10 -0300

    feat: ajustando controller de verificacao de email sem a necessidade do usuario logar

--- app/Http/Controllers/Auth/VerifyEmailController.php
+++ app/Http/Controllers/Auth/VerifyEmailController.php
@@ -9,24 +9,29 @@ use Illuminate\Auth\Events\Verified;
 use Illuminate\Foundation\Auth\EmailVerificationRequest;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
 class VerifyEmailController extends Controller
 {
     //****** */ modo redirect
-    public function __invoke(EmailVerificationRequest $request): RedirectResponse
+    // se quiser que usuário esteja autenticado p/ verificar o email
+    // public function __invoke(EmailVerificationRequest $request): RedirectResponse
+
+    // se nao quiser que usuario esteja autenticado p/ verificar o email
+    public function __invoke(Request $request): RedirectResponse
     {
         $user = $request->route('id') ? User::find($request->route('id')) : null;
 
         if (!$user || $user->hasVerifiedEmail()) {
-            return redirect()->route('verified', ['verified' => 0]);
+            return redirect()->route('verified', ['verified' => 1]);
         }
 
         if ($user->markEmailAsVerified()) {
             event(new Verified($user));
         }
 
-        return redirect()->route('verified', ['verified' => 1]);
+        return redirect()->route('verified', ['verified' => 0]);
     }
 
 
