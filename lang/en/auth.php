cff44f2c0de846b47a65020fa8c2a19a799ab291
rafaxavier <rxn.90s@gmail.com>
2023-11-27 00:38:54 -0300

    feat: traduzindo msg par pt

--- lang/en/auth.php
+++ lang/en/auth.php
@@ -13,8 +13,13 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'password' => 'The provided password is incorrect.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    // 'failed' => 'These credentials do not match our records.',
+    // 'password' => 'The provided password is incorrect.',
+    // 'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+
+    'failed' => 'Essas credenciais não correspondem aos nossos registros.',
+    'password' => 'A senha fornecida está incorreta.',
+    'throttle' => 'Muitas tentativas de login. Por favor, tente novamente em :seconds segundos.',
+
 
 ];
---
+++ lang/en/messages.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'reset_password_instruction' => 'Se você estiver com dificuldades para clicar no botão "Redefinir Senha", copie e cole a URL abaixo em seu navegador da web:',
+];
