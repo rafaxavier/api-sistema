1f176949c192744fc75b57054cacfa1d3b411a8f
rafaxavier <rxn.90s@gmail.com>
2023-11-27 00:40:01 -0300

    feat: impelentando envio de notificacao de reset de senha em User

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Notifications\ResetPasswordNotification;
 use Illuminate\Auth\MustVerifyEmail;
 use Illuminate\Contracts\Auth\MustVerifyEmail as MustVerifyEmailContract;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
@@ -44,4 +45,9 @@ class User extends Authenticatable implements MustVerifyEmailContract
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function sendPasswordResetNotification($token)
+    {   
+        $this->notify(new ResetPasswordNotification($token, $this->name));
+    }
 }
